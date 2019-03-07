965003206f6fe24ab008246d2ef81d9ea48c25b7
ahaxiong <ahaxiong@163.com>
2019-03-08 00:40:21 +0800

    将 svg 改为 icon 组件

--- src/button.vue
+++ src/button.vue
@@ -1,29 +1,38 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <g-icon v-if="icon" :name="icon"></g-icon>
+        <div class="content">
+            <slot>按钮</slot>
+        </div>
+    </button>
 </template>
 
 <script>
     export default {
-        name: "button"
+        name: "button",
+        props:{
+            icon:{},
+            iconPosition:{
+                type:String,
+                default:'left',
+                validator: function (value) {
+                    return value === 'left' || value === 'right'
+                }
+            }
+        }
     }
 </script>
 
 <style lang="scss">
-    .g-button {
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
-        &:hover {
-            border-color: var(--border-color-hover);
-        }
-        &:active {
-            background-color: var(--button-active-bg);
-        }
-        &:focus {
-            outline: none;
+    .g-button {font-size: var(--font-size);height: var(--button-height);padding: 0 1em;border-radius: var(--border-radius);border: 1px solid var(--border-color);background: var(--button-bg);display: inline-flex;justify-content: center;align-items: center;vertical-align: top;
+        &:hover {border-color: var(--border-color-hover);}
+        &:active {background-color: var(--button-active-bg);}
+        &:focus {outline: none;}
+        > .icon {order: 1;margin-right: 0.1em;}
+        > .content {order: 2;}
+        &.icon-right {
+            .icon {order: 2;margin-left: 0.1em;margin-right: 0;}
+            .content {order: 1;}
         }
     }
 
